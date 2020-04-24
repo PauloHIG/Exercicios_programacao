a9cda20db3b80569463c9f62f10591da61fa94c3
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-04-24 14:00:59 -0300

    Add files via upload

---
+++ hue.py
@@ -0,0 +1,3 @@
+brinks = 'odfg sefcbgfofgbrfgbnsawoasd vfdtdfvádas asdlasdebfdncvgddrhosds 123k123k123k123k123k123k'
+print(brinks[::4])
+#copiei a bricandeira de um comentário do youtube, tá aí mais uma função no python que nunca mais vou esqueçer hehehe
