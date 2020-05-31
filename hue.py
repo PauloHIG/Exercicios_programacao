5af244ef4c4acf38e6698e722c0896304d3fe21d
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-05-31 18:35:11 -0300

    Delete hue.py

--- hue.py
+++
@@ -1,4 +0,0 @@
-brinks = 'os ecgournndoa  tmáe nlseangdeom  kgkêknki o'
-print(brinks[::2])
-#copiei a bricandeira de um comentário do youtube, tá aí mais uma função no python que nunca mais vou esqueçer hehehe
-#tem um easter egg aí, descubra seja lá quem estiver lendo isso aqui
