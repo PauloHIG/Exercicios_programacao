44e94986ef6ddd938c0b08c765c2ab75fd7d1ca2
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-06-02 18:44:41 -0300

    Add files via upload

---
+++ prints_com_varias_mensagens.py
@@ -0,0 +1,6 @@
+#uma brincadeira pra entender como usar (::)
+mensagem = 'OSGleOágS uTMnOud naDd Eom  eB nA sT aA gT eA m '
+print(mensagem[2::3])
+'''como eu escrevi três mensagens em um texto, printro sempre pulando de 3 em e(::3)
+para escolher qual mensagem, eu escolho de onde vai começar, com (1::3) por exemplo eu
+pego a segunda mensagem porque ele pula o primeiro caractere e depois lê pulando de 3 em 3'''
