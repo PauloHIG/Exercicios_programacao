5687980f65c48df6da62c2a652ea968dba64608e
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-08-04 21:54:09 -0300

    Add files via upload

--- somaVF.py
+++ somaVF.py
@@ -1,7 +1,13 @@
 #mathgame
 #a idéia é clonar um jogo de celular em que você vê uma soma de um número e o resultado, e verifica se o resultado está certo ou errado com limite de tempo
 from random import randint
+import sys
+
 recordeSoma = 0
+recordeSub  = 0
+recordeMulti = 0
+recordeDiv = 0
+
 def jogada():
 	while True:#mais um try para o usuario digitar o que é pedido sem interromper o programa
 		try:
@@ -20,10 +26,10 @@ def escolha():
 		try:
 			escolha = int(input(""))
 		except:
-			print('Somente um número de 1 a 5')
+			print('Somente as opções de 1 a 5')
 			continue
 		if escolha not in (1,2,3,4,5):
-			print('Somente um único número de 1 a 5')
+			print('Somente as opções de 1 a 5')
 			continue
 		break
 	return escolha
@@ -39,7 +45,7 @@ def jogoSoma():
 
 		if dec == 1:#caso seja 0, a resposta não será alterada, se mantendo verdadeira
 			#resposta que poderá ser verdadeira ou falsa se for igual a soma de n1 + n2
-			ref = randint(-2,2)
+			ref = randint(0,4)
 
 		if n1+n2 == n1+n2+ref:
 			certo = 'V'
@@ -56,11 +62,88 @@ def jogoSoma():
 			contaAcertos += 1
 		if contaAcertos > recordeSoma:
 			recordeSoma = contaAcertos
+def jogoSub():
+	contaAcertos = 0
+	global recordeSub
+	while True:
+		n1 = randint(1,10)
+		n2 = randint(1,10)
+		dec = randint(0,1)#uma tentativa de fazer com que 50% das respostas seja a certa
+		ref=0
 
+		if dec == 1:
+			ref = randint(0,4)#o ref é a variável que decide se a conta vai ficar verdadeira ou falsa
+		if n1-n2 == n1-n2+ref:
+			certo = 'V'
+		else:
+			certo = 'F'
+		print('pontuação atual: {} recorde:{}'.format(contaAcertos,recordeSub))#o récorde ainda não é gravado corretamente, ele desaparece quando o programa é fechado
+		print('Esta conta está certa?')
+		print('{} - {} = {}'.format(n1,n2,n1-n2+ref))
+		resposta = jogada()
+		if resposta.upper() != certo:
+			print('você errou, fim de jogo')
+			break
+		else:
+			contaAcertos += 1
+		if contaAcertos > recordeSub:
+			recordeSub = contaAcertos
+def jogoMulti():
+	contaAcertos = 0
+	global recordeMulti
+	while True:
+		n1 = randint(1,10)
+		n2 = randint(1,10)
+		dec = randint(0,1)#uma tentativa de fazer com que 50% das respostas seja a certa
+		ref=0
 
+		if dec == 1:
+			ref = randint(0,4)#o ref é a variável que decide se a conta vai ficar verdadeira ou falsa
+		if n1*n2 == (n1*n2)+ref:
+			certo = 'V'
+		else:
+			certo = 'F'
+		print('pontuação atual: {} recorde:{}'.format(contaAcertos,recordeMulti))#o récorde ainda não é gravado corretamente, ele desaparece quando o programa é fechado
+		print('Esta conta está certa?')
+		print('{} * {} = {}'.format(n1,n2,(n1*n2)+ref))
+		resposta = jogada()
+		if resposta.upper() != certo:
+			print('você errou, fim de jogo')
+			break
+		else:
+			contaAcertos += 1
+		if contaAcertos > recordeMulti:
+			recordeMulti = contaAcertos
+def jogoDiv():
+	contaAcertos = 0
+	global recordeDiv
+	while True:
+		n1 = randint(1,10)
+		n2 = randint(1,10)
+		dec = randint(0,1)#uma tentativa de fazer com que 50% das respostas seja a certa
+		ref=0
+
+		if dec == 1:
+			ref = randint(0,4)#o ref é a variável que decide se a conta vai ficar verdadeira ou falsa
+		if n1/n2 == (n1//n2)+ref:
+			certo = 'V'
+		else:
+			certo = 'F'
+		print('pontuação atual: {} recorde:{}'.format(contaAcertos,recordeDiv))#o récorde ainda não é gravado corretamente, ele desaparece quando o programa é fechado
+		print('Esta conta está certa?')
+		print('{} / {} = {}'.format(n1,n2,(n1//n2)+ref))
+		resposta = jogada()
+		if resposta.upper() != certo:
+			print('você errou, fim de jogo')
+			break
+		else:
+			contaAcertos += 1
+		if contaAcertos > recordeDiv:
+			recordeDiv = contaAcertos
 
 while True:
 	print('------Jogo de Matemática-------')
+	print('Recordes: Soma = {}, Subtração = {}, Multiplicação = {}, Divisão = {}'.format(recordeSoma, recordeSub, recordeMulti, recordeDiv))
 	print('Escolha uma operação')
 	print('1-Soma')
 	print('2-Subtração')
@@ -71,11 +154,12 @@ while True:
 	if Escolha == 1:
 		jogoSoma()
 	elif Escolha == 2:
-		print('Em breve')
+		jogoSub()
 	elif Escolha == 3:
-		print('Em breve')
+		jogoMulti()
 	elif Escolha == 4:
-		print('Em breve')
+		jogoDiv()
 	elif Escolha == 5:
 		print('Saindo do programa...')
+		sys.exit()
 		break
